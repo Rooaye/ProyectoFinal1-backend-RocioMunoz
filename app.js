f9eb951655fe1b068514b4f8ed446dc2965e56db
Rocio Muñoz <ayelen.ro.96m@gmail.com>
2023-10-10 23:03:47 -0300

    arreglo ruta

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import productRouter from '../routers/products.js'
+import productRouter from '../routers/product.js'
 import cartRouter from '../routers/cart.js'
 const app = express()
 const port = 8080;
