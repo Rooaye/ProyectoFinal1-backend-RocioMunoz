d1eaab420a7341438125fd3af22b9d5d35684339
Rocio Muñoz <ayelen.ro.96m@gmail.com>
2023-10-10 22:40:09 -0300

    Proyecto final parte 1

---
+++ routers/cart.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+
+const router = Router();
+
+const db = {};
+
+router.post('/', (req, res) => {
+  const cartId = cartId.length + 1; //
+  db[cartId] = { id: cartId, products: [] };
+  res.status(201).json(db[cartId]); //
+});
+
+router.get('/:cid', (req, res) => {
+  const cart = db[req.params.cid];
+  if (!cart) {
+    res.status(404).json({ message: 'Carrito no encontrado' });
+  } else {
+    res.json(cart.products);
+  }
+});
+
+router.post('/:cid/product/:pid', (req, res) => {
+  const cart = db[req.params.pid];
+  if (!cart) {
+    res.status(404).json({ message: 'Carrito no encontrado' });
+  } else {
+    const productId = req.params.pid;
+    const product = { id: productId, quantity: 1 };
+    const existingProduct = cart.products.find(p => p.id === productId);
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      cart.products.push(product);
+    }
+    res.json(cart.products);
+  }
+});
+
+
+export default router;
